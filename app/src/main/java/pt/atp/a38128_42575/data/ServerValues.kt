867ab7c7674276909b2223de40b1fdd691bacfae
MiguelPortugal <56307654+MiguelPortugal@users.noreply.github.com>
2022-05-18 18:55:03 +0100

    List mas ainda nao esta correto

--- app/src/main/java/pt/atp/a38128_42575/data/ServerValues.kt
+++ app/src/main/java/pt/atp/a38128_42575/data/ServerValues.kt
@@ -2,5 +2,5 @@ package pt.atp.a38128_42575.data
 
 internal const val BASE_URL= "https://api.thecatapi.com/"
 
-internal const val API_KEY = "207a52ba-8e07-4d5f-ae6b-3ff0482eba53"
+internal const val API_KEY = "991e0a29-2e1a-4065-bcc7-81bbbbf72c62"
 internal const val BREEDS= "v1/breeds"
